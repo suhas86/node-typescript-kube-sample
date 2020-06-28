7146727e07859d0005836588f384e4f9d90df489
Suhas <murthy.suhas@gmail.com>
2020-06-29 01:14:01 +0530

    test env setup

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("Should not allow to create user without first name", () => {});
+
+it("Should not allow to create user with invalid email", () => {});
+
+it("Should not allow to create user with invalid password", () => {});
+
+it("Should not allow to create user without email id and password", () => {});
+
+it("Should not allow to create user without same password and confirm password", () => {});
+
+it("Should not allow to create user with same email", () => {});
+
+it("Should allow to create user with proper first name, last name  email, password and confirm password", () => {});
+
+it("Should allow to create user without last name", () => {});
